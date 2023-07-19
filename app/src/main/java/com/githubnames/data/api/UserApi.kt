5c0954b48b007d32d17f3b517b3283fd9bbeef41
Iago Novoa <iago.novoa.antunes@gmail.com>
2023-07-19 11:49:48 -0300

    Create getAllUsers function with the right return

--- app/src/main/java/com/githubnames/data/api/UserApi.kt
+++ app/src/main/java/com/githubnames/data/api/UserApi.kt
@@ -1,11 +1,10 @@
 package com.githubnames.data.api
 
+import com.githubnames.data.dto.UserDTO
 import retrofit2.http.GET
 
 interface UserApi {
 
     @GET("/users")
-    suspend fun getAllUsers() {
-
-    }
+    suspend fun getAllUsers(): List<UserDTO>
 }
