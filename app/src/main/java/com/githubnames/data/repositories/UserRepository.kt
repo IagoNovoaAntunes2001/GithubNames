debf9a78497cd644c750ddf0724e8dbafe8d8841
Iago Novoa <iago.novoa.antunes@gmail.com>
2023-07-19 11:57:15 -0300

    Created repository interface method and repository implementation doing the implementation calling userApi method.

--- app/src/main/java/com/githubnames/data/repositories/UserRepository.kt
+++ app/src/main/java/com/githubnames/data/repositories/UserRepository.kt
@@ -1,4 +1,7 @@
 package com.githubnames.data.repositories
 
+import com.githubnames.data.dto.UserDTO
+
 interface UserRepository {
+    suspend fun getAllUsers(): List<UserDTO>
 }
--- app/src/main/java/com/githubnames/domain/user/UserRepositoryImpl.kt
+++ app/src/main/java/com/githubnames/domain/user/UserRepositoryImpl.kt
@@ -1,5 +1,10 @@
 package com.githubnames.domain.user
 
-class UserRepositoryImpl {
+import com.githubnames.data.api.UserApi
+import com.githubnames.data.repositories.UserRepository
 
+class UserRepositoryImpl(
+    private val userApi: UserApi
+) : UserRepository {
+    override suspend fun getAllUsers() = userApi.getAllUsers()
 }
