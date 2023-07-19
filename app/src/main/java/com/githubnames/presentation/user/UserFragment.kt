8f263e6a3367e4178cd9aa01828fee6ee98d554c
Iago Novoa <iago.novoa.antunes@gmail.com>
2023-07-19 12:22:47 -0300

    Initial configuration of navigation has made

--- app/src/main/java/com/githubnames/presentation/user/UserFragment.kt
+++ app/src/main/java/com/githubnames/presentation/user/UserFragment.kt
@@ -1,18 +1,31 @@
 package com.githubnames.presentation.user
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.githubnames.R
+import com.githubnames.databinding.FragmentUserBinding
 
-class UserFragment : Fragment() {
+class UserFragment : Fragment(R.layout.fragment_user) {
+
+    private var _binding: FragmentUserBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_user, container, false)
+    ): View {
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+
+        }
     }
 }
