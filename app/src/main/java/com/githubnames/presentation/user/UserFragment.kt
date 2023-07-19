453e603f2e37482c8b544a257d90facafa7efcec
Iago Novoa <iago.novoa.antunes@gmail.com>
2023-07-19 12:08:17 -0300

    Created initial fragment and added dependencie of navigation component

---
+++ app/src/main/java/com/githubnames/presentation/user/UserFragment.kt
@@ -0,0 +1,18 @@
+package com.githubnames.presentation.user
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.githubnames.R
+
+class UserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+}
